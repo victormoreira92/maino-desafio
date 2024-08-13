327621caea2a371b2d512a6919e2539a9205e9b0
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-08-13 17:17:51 -0300

    fix: Correção do nome da rota de 'home_index/index' para 'home'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
-  get 'home_index/index'
+  get 'home' => 'home_index#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
