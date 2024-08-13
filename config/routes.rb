2fa9fe32e2cc93235505d0f2681ed78818d6f7e6
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-08-13 17:02:17 -0300

    feature: Criação de HomeIndexController e DashboardController assim como suas views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  get 'home_index/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'dashboard#index'
 end
