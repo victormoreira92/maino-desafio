df8adf10a4e553a446f3d5520b8fcdffc573512d
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-08-14 19:13:21 -0300

    build: Adição de jquery e jquey-validation no projeto

---
+++ app/javascript/validation_form.js
@@ -0,0 +1,22 @@
+function usuarioFormValidation(){
+    console.log("funcionou");
+
+}
+
+$(document).ready(function () {
+    $.validator.setDefaults({
+        errorElement: 'span',
+        errorPlacement: function (error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.input-group').append(error);
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).addClass('is-invalid');
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        }
+    });
+
+    usuarioFormValidation();
+});
