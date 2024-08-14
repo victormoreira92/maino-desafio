29a678a2839743aceb1d7a9e1f2d752abb9fc138
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-08-13 23:13:54 -0300

    feat: Adição de validação de atributos de Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,18 @@
 class Usuario < ApplicationRecord
+  before_validation :set_perfil, on: :create
 
   enum perfil: {
     admin: 0,
     cliente: 1
   }
+
+  validates :nome, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :nome, length: { minimum: 2, maximum: 75 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def set_perfil
+    self.perfil = :cliente if self[:perfil].blank?
+  end
+
 end
