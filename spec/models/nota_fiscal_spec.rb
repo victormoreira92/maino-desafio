23ddf8927d273e71b9bc5525de3fdae22ad460c8
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-08-16 17:50:35 -0300

    test: Testes de modelo Notafiscal com teste de documento

--- spec/models/nota_fiscal_spec.rb
+++ spec/models/nota_fiscal_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe NotaFiscal, type: :model do
           expect(nota_fiscal.errors[atributo]).to include('não pode ficar em branco')
         end
       end
+      it 'sem documento' do
+        nota_fiscal = build(:nota_fiscal, documento: nil)
+        nota_fiscal.valid?
+        expect(nota_fiscal.errors[:documento]).to include('é obrigatório(a)')
+      end
     end
   end
 end
