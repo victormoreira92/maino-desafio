b77aa951bfe94d5cc1db99c10940b152d9f37bff
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-02-10 17:37:50 -0300

    test: Configurações de testes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'faker'
-require 'capybara/rspec'
 require 'active_storage_validations/matchers'
 
 RSpec.configure do |config|
