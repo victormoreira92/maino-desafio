785403899ef5d76cdeded17364776e20f43710bd
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-02-10 18:02:59 -0300

    refactor: Corrigindo as ações no UsuarioController

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -3,35 +3,28 @@ class UsuariosController < ApplicationController
   before_action :set_usuario, only: %i[show edit update destroy]
 
 
-  # GET /usuarios/1 or /usuarios/1.json
-  def show
-  end
+  def show ; end
 
-  # GET /usuarios/new
   def new
     @usuario = Usuario.new
   end
 
-  # GET /usuarios/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /usuarios or /usuarios.json
   def create
     @usuario = Usuario.new(usuario_params)
 
     respond_to do |format|
       if @usuario.save
-        format.html { redirect_to usuario_url(@usuario), notice: "Usuario was successfully created." }
-        format.json { render :show, status: :created, location: @usuario }
+        flash[:success] = t('activerecord.success.messages.create', model: Usuario.model_name.human)
+        format.html { redirect_to usuario_url(@usuario)}
       else
+        flash[:error] = @usuario.errors.full_messages
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @usuario.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /usuarios/1 or /usuarios/1.json
   def update
     respond_to do |format|
       if @usuario.update(usuario_params)
@@ -44,15 +37,7 @@ class UsuariosController < ApplicationController
     end
   end
 
-  # DELETE /usuarios/1 or /usuarios/1.json
-  def destroy
-    @usuario.destroy
 
-    respond_to do |format|
-      format.html { redirect_to usuarios_url, notice: "Usuario was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -62,6 +47,6 @@ class UsuariosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def usuario_params
-      params.require(:usuario).permit(:email, :nome, :perfil, :password, :password_confirmation)
+      params.require(:usuario).permit(:email, :nome, :password, :password_confirmation)
     end
 end
